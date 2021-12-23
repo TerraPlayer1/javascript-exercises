d44fb4fe6780db58c54f3ebae740edfac4141341
TerraPlayer1 <terragamer70@gmail.com>
2021-12-23 19:29:58 +0200

    Finish exercise 4

---
+++ 04_removeFromArray/pseudoCode.md
@@ -0,0 +1,3 @@
+take array and value to be removed
+filter out value to be removed
+return array with removed values
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let filterdArray = array.filter(value => !args.includes(value));
+    return filterdArray
 };
 
 // Do not edit below this line
