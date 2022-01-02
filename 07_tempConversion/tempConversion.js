d50a42281b519864e5d55b113bdb48f318f90aab
TerraPlayer1 <terragamer70@gmail.com>
2022-01-02 20:26:03 +0200

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+    return Math.round(((F - 32) * 5/9) * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+    return Math.round((C * 9/5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
