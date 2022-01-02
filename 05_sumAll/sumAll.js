ac569f7d24a8489a9431ff555827ea4625482615
TerraPlayer1 <terragamer70@gmail.com>
2022-01-02 19:22:23 +0200

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(v1,v2) {
+    let sum = 0
+    if ((v1 < -1 || v2 < -1) || (typeof v1 != "number" || typeof v2 != "number" )){
+        return "ERROR"
+    }else if (v1 < v2){
+        for (let i = v1; i <= v2; i++){
+            sum += i;
+        };
+    }else if (v1 > v2){
+        for (let i = v2; i <= v1; i++){
+            sum += i;
+        };
+    }else return "YOU WIN"
+    return sum;
 };
 
 // Do not edit below this line
