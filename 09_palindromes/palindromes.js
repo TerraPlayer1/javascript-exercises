837379274efc69fb921748e90c5bcddb502e2111
TerraPlayer1 <terragamer70@gmail.com>
2022-04-30 17:00:54 +0300

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let clnStr = str.match(/([A-z])+/g).join("").toLowerCase();
+  let revStr = str.match(/([A-z])+/g).join("").split("").reverse().join("").toLowerCase();
+  return revStr === clnStr;
 };
 
 // Do not edit below this line
