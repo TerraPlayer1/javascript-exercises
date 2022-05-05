c1a34deee65bacbb2057f4f1e8b4f6ddf795ebde
TerraPlayer1 <terragamer70@gmail.com>
2022-05-05 17:12:20 +0300

    Fix poor if statement
    After finishing previous version I checked the actual solution
    Realized flaw in lack of death date check
    Took a small break and rewrote code based on actual solution
    from memory

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,18 @@
 const findTheOldest = function(people) {
 
-  if (people[0].yearOfDeath === undefined){
-    let currYear = new Date().getFullYear();
-    people[0].yearOfDeath = currYear;
-    return people.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
-  }else {
-    return people.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
-  };
+  return people.reduce((a, b) => {
+    const old = calcYears(a.yearOfBirth, a.yearOfDeath);
+    const newPerson = calcYears(b.yearOfBirth, b.yearOfDeath);
+    return old < newPerson ? b : a;
+  });
 };
+
+const calcYears = function(birth, death){
+  if(!death){
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
 // Do not edit below this line
 module.exports = findTheOldest;
