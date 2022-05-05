a8391e2f560478af9ccc1abacefa6664d0cf4b10
TerraPlayer1 <terragamer70@gmail.com>
2022-05-05 17:12:20 +0300

    Solve using reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,12 @@
 const findTheOldest = function(people) {
-  
+
   if (people[0].yearOfDeath === undefined){
     let currYear = new Date().getFullYear();
     people[0].yearOfDeath = currYear;
-    const oldPerson = people.sort((a, b) =>
-     (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
-     return oldPerson[0]
-     
-  } else {
-    const oldPerson = people.sort((a, b) =>
-     (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
-    return oldPerson[0]
+    return people.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
+  }else {
+    return people.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? a : b);
   };
 };
-//    const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
-
 // Do not edit below this line
 module.exports = findTheOldest;
