d85fcd52f0f00da449a72257c9593cadc1e3d539
TerraPlayer1 <terragamer70@gmail.com>
2022-05-05 17:14:33 +0300

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  
+  if (people[0].yearOfDeath === undefined){
+    let currYear = new Date().getFullYear();
+    people[0].yearOfDeath = currYear;
+    const oldPerson = people.sort((a, b) =>
+     (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+     return oldPerson[0]
+     
+  } else {
+    const oldPerson = people.sort((a, b) =>
+     (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return oldPerson[0]
+  };
 };
+//    const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
 
 // Do not edit below this line
 module.exports = findTheOldest;
